f61b38ff3db620cdfd185f37d5281b4d88e8f69f
xuefeng <t-ink@qq.com>
2026-06-03 09:27:09 +0800

    增加辅助脚本

---
+++ gear_sonic_deploy/scripts/get_nav2_param.sh
@@ -0,0 +1,5 @@
+# acc_lim_x、acc_lim_theta、decel_lim_x、decel_lim_theta
+ros2 param get /controller_server FollowPath.acc_lim_x
+ros2 param get /controller_server FollowPath.acc_lim_theta
+ros2 param get /controller_server FollowPath.decel_lim_x
+ros2 param get /controller_server FollowPath.decel_lim_theta
